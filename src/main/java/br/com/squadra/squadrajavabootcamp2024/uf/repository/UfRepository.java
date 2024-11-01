5cf17127381464d504cd38f9b438562d5dfd7ede
Danilo Chaves <danchavesds@gmail.com>
2024-10-31 22:31:07 -0300

    feat: adicionando repostiry uf

---
+++ src/main/java/br/com/squadra/squadrajavabootcamp2024/uf/repository/UfRepository.java
@@ -0,0 +1,18 @@
+package br.com.squadra.squadrajavabootcamp2024.uf.repository;
+
+import br.com.squadra.squadrajavabootcamp2024.uf.model.UfModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UfRepository  extends JpaRepository<UfModel, Long> {
+
+    List<UfModel> findAllByOrderByIdDesc();
+
+    Optional<UfModel> findByCodigoUFOrSiglaOrNome(Long codigoUF, String sigla, String nome);
+
+    List<UfModel> findByStatus(Integer status);
+}
