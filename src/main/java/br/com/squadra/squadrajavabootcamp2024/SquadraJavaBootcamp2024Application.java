599304447ac7f7479be38e2e899027e72788a73b
Danilo Chaves <danchavesds@gmail.com>
2024-10-31 22:13:12 -0300

    iniciando projeto

---
+++ src/main/java/br/com/squadra/squadrajavabootcamp2024/SquadraJavaBootcamp2024Application.java
@@ -0,0 +1,13 @@
+package br.com.squadra.squadrajavabootcamp2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadraJavaBootcamp2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquadraJavaBootcamp2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/squadra/squadrajavabootcamp2024/SquadraJavaBootcamp2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.squadra.squadrajavabootcamp2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadraJavaBootcamp2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
